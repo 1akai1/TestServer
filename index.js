958e079762ff0e59e6d5d9613fa683fc39af2c8b
1akai1 <2akai@mail.ru>
2023-08-22 20:13:45 +0400

    fixGeneral

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.json())
+app.use(cors())
 
-// const posts = require('./routes/api/posts');
-// app.use('/api/posts', posts);
+const posts = require('./routes/api/posts')
 
-app.post('/api/data', (req, res) => {
-  const newData = req.body;
-  res.json({ message: 'Данные успешно добавлены' });
-});
+app.use('/api/posts', posts)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
-});
+app.listen(port, () => console.log(`Server stared on port ${port}`))
+
+// app.post('/api/data', (req, res)=>{
+//     const newData = req.body
+
+//     res.json({message: 'Данные успешно добавлены'})
+// })
+
+// const port = 3000
+
+// app.listen(port,()=>{
+//     console.log(`Сервер запущен на порту ${port}`)})
