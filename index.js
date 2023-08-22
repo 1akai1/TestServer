c3d07a54a59113fab4dec75ad5c72e7d37535603
1akai1 <2akai@mail.ru>
2023-08-22 15:51:59 +0400

    test yml

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
-// const posts = require('./routes/api/posts')
+// const posts = require('./routes/api/posts');
+// app.use('/api/posts', posts);
 
-// app.use('/api/posts', posts)
+app.post('/api/data', (req, res) => {
+  const newData = req.body;
+  res.json({ message: 'Данные успешно добавлены' });
+});
 
-// const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000;
 
-// app.listen(port, () => console.log(`Server stared on port ${port}`))
-
-app.post('/api/data', (req, res)=>{
-    const newData = req.body
-
-    res.json({message: 'Данные успешно добавлены'})
-})
-
-const port = 3000
-
-app.listen(port,()=>{
-    console.log(`Сервер запущен на порту ${port}`)})
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
